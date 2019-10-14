3a2bd2d801ba18e21a33e86a839e7d801039141a
lowjiansheng <lowjiansheng@hotmail.sg>
2019-10-15 00:56:24 +0800

    update alignment for player setup cards

--- src/shared/layouts/PlayerSetupLayout/LocationSpyContent.js
+++ src/shared/layouts/PlayerSetupLayout/LocationSpyContent.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { CardContent, Typography, Button } from "@material-ui/core";
 
+import { makeStyles } from '@material-ui/styles';
+
 export const LocationSpyContent = ({handleLocationSpyRevealClick, location, isSpy}) => {
+    const classes = useStyles();
+
     var displayContent;
     if (isSpy) { 
         displayContent = <span>You are a Spy.</span>
@@ -9,13 +13,33 @@ export const LocationSpyContent = ({handleLocationSpyRevealClick, location, isSp
         displayContent = <span>The location is {location}.</span>
     }
     return (
-        <CardContent>
-            <Typography className="d-flex p2" variant="body2">
+        <CardContent className={classes.cardContent}>
+            <Typography className={classes.typographyAlign} variant="body2">
                 {displayContent}
             </Typography>
-            <Typography className="d-flex p2 justify-content-center" variant="body2">
+            <Typography className={classes.typographyAlign} variant="body2">
                 <Button variant="contained" color="primary" onClick={handleLocationSpyRevealClick}>Done</Button>
             </Typography>
         </CardContent> 
     )
 }
+
+const useStyles = makeStyles({
+    cardContent: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
+    },
+    root: {
+        flex: 1,
+        display: "flex",
+        justifyContent: "center"
+    },
+    typographyAlign:{
+        alignSelf: "center",
+        margin: "5%"
+    },
+    typographyStyle: {
+        margin: "5%"
+    }
+});
